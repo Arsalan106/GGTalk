7e633cb0239802eb675d3a809344e858a43144ac
Arsalan106 <ahmedarsalan0775@gmail.com>
2025-05-29 22:03:34 +0530

    updated for vercel

--- server/lib/cloudinary.js
+++ server/lib/cloudinary.js
@@ -1,7 +1,7 @@
 import { v2 as cloudinary} from "cloudinary";
 
 cloudinary.config({
-    cloud_name:process.env.CLOUDINARY_CLOUD_NME,
+    cloud_name:process.env.CLOUDINARY_CLOUD_NAME,
     api_key:process.env.API_KEY,
     api_sec:process.env.API_SECRET,
 })
